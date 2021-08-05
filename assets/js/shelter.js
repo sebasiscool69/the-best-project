a9524f44dcb344cd2ac95a4ce422cbd35c9eb579
Biggy <bilalersoy740@gmail.com>
2021-08-05 15:19:15 -0400

    added api info

--- assets/js/shelter.js
+++ assets/js/shelter.js
@@ -1,31 +1,10 @@
-var soupIn = document.querySelector("#soupCheck");
-var shelIn = document.querySelector("#shelCheck");
-var volIn = document.querySelector("#volCheck");
-var btn = document.querySelector("#buttonSubmit");
-var formEl = document.querySelector("#startUp");
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
 
-//Feching Soup Kitchen models from the api
-
-fetch("https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Public_Service_WebMercator/MapServer/25/query?where=1%3D1&outFields=*&outSR=4326&f=json")
-.then(response => response.json()).then(data => console.log(data))
+//Feching shelters from api
+fetch("https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Public_Service_WebMercator/MapServer/25/query?where=1%3D1&outFields=OWNER_RENTER,FACILITY_NAME,PROVIDER,ADDRESS,ZIP,WARD,TYPE,SUBTYPE,NUMBER_OF_BEDS,ON_SITE_MEDICAL_CLINIC,URL,ALREADY_PUBLIC_INFORMATION,AGES_SERVED&outSR=4326&f=json")
+.then(response => response.json())
+.then(data => console.log(data))
 
 
-function stepOne() {
-    
-    
-}
-
-btn.onclick = function stepOne() {
-
-}
-
-//formEl.addEventListener("Submit", e => {
-//    var formData = {
-//        soupKitchen: soupIn.addEventListener("click"),
-//        shelterHome: shelIn.addEventListener("click"),
-//        volunteer: volIn.addEventListener("click")
-//    }
-//    localStorage.setItem('formData', JSON.stringify(formData));
-//    e.preventDefault();
-//});
-//console.log(localStorage.getItem('formData'));
